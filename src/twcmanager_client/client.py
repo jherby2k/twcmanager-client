d97da38f54cda525e07282aeaff8fc9b7acf995b
Jeremy Herbison <jeremy.herbison@gmail.com>
2021-07-16 11:16:55 -0600

    Expect the host only include the hostname (not http prefix or port)

--- src/twcmanager_client/client.py
+++ src/twcmanager_client/client.py
@@ -12,22 +12,22 @@ class TWCManagerClient:
 
     async def async_get_config(self) -> dict[str, Any]:
         """Get the current configuration."""
-        return await self._async_get(self._host + '/api/getConfig')
+        return await self._async_get("http://" + self._host + ':8080/api/getConfig')
     
 
     async def async_get_policy(self) -> dict[str, Any]:
         """Get the policy configuration."""
-        return await self._async_get(self._host + '/api/getPolicy')
+        return await self._async_get("http://" + self._host + ':8080/api/getPolicy')
 
 
     async def async_get_slave_twcs(self) -> dict[str, Any]:
         """Get a list of connected slave TWCs and their state."""
-        return await self._async_get(self._host + '/api/getSlaveTWCs')
+        return await self._async_get("http://" + self._host + ':8080/api/getSlaveTWCs')
 
 
     async def async_get_status(self) -> dict[str, Any]:
         """Get the current status (charge rate, policy)."""
-        return await self._async_get(self._host + '/api/getStatus')
+        return await self._async_get("http://" + self._host + ':8080/api/getStatus')
     
 
     async def _async_get(self,path: str,) -> dict[str, Any]:
